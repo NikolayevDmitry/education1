c3642ddd3e7b3eb8f9d91d918ea0419745fab494
NikolayevDmitry <NikolayevDmitry@yandex.ru>
2017-12-07 00:52:28 +0300

    aut

--- app.js
+++ app.js
@@ -1,45 +1,114 @@
 //-------------------------------<MODULES>--------------------------------
 const Koa = require('koa')
 const Router = require('koa-router')
+const Sequelize = require('sequelize')
 const views = require('koa-views')
 const static = require('koa-static')
-const onerror = require('koa-onerror')
-const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
+const bodyparser = require('koa-bodyparser')
+const onerror = require('koa-onerror')
 const path = require('path')
-
-const config = {port : 8000}
+const env = process.env.NODE_ENV || 'development'
+const config = require(__dirname + '/config.json')[env]
+const port = process.env.PORT || config.port
 //------------------------------</MODULES>--------------------------------
 
 //--------------------------------<CONFIG>--------------------------------
 const app = new Koa()
 const router = new Router()
-const port = process.env.PORT || config.port
 onerror(app)
-app.use(bodyparser())
-app.use(static('__dirname + /public'))
 app.use(logger())
+app.use(static(__dirname + '/public'))
 app.use(views(path.join(__dirname, '/views'), {
   options: {settings: {views: path.join(__dirname, 'views')}},
   map: {'njk': 'nunjucks'},
   extension: 'njk'
 }))
+app.use(bodyparser())
 
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  {
+    host: config.host,
+    dialect: config.dialect,
+    logging: console.log,
+    operatorsAliases: false,
+    freezeTableName: true
+  }
+);
+sequelize.authenticate()
+.then(() => console.log(
+  `Connection with the ${config.dialect} database established.`
+))
+.catch(err => console.log(
+  `Unable to connect to the ${config.dialect} database: `, err
+));
 //-------------------------------</CONFIG>--------------------------------
 
 //---------------------------------<MODEL>--------------------------------
+var User = sequelize.define('user',
+{
+  username: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false
+  },
+  fullname: {
+    type: Sequelize.STRING
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+});
+
+User.sync({force: true})
+  .then(() => console.log('All models were mapped successfully.'))
+  .catch(err => console.log('Unable to map the models. ', err));
 //--------------------------------</MODEL>--------------------------------
 
 //-----------------------------<CONTROLLER>-------------------------------
-router.get('/users/:id', (ctx, next) => {
-  ctx.body = "Hello user number " + ctx.params.id
- })
+router.get('/testdb', ctx => {
+  User.create({
+    username: 'user',
+    fullname: 'user',
+    email: 'user@ya.ru',
+    password: 'u'
+  });
+  User.create({
+    fullname: 'admin',
+    username: 'admin',
+    email: 'admin@ya.ru',
+    password: 'a'
+  });
+  ctx.redirect('/');
+});
 
-router.get('/', (ctx, next) => {
-  ctx.body = "Hello world"
+router.use((ctx, next) => {
+  return next().catch(err => {
+    ctx.status = err.status || 500;
+    return ctx.render('pages/error404', {
+      message: err.message,
+      error: err
+    });
+  })
+});
+
+app.use(async ctx => {
+  await ctx.render('pages/error404')
+});
+
+router.get('/', async ctx => {
+  await ctx.render('pages/home')
 })
 
-app.use(router.routes())
+app.use(router.routes(), router.allowedMethods())
 
 app.listen(port, 8000)
 //----------------------------</CONTROLLER>-------------------------------
